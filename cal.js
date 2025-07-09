d88ed98e23deae105882074a2c9313b56fbf10d0
srijitadeogharia <srijitadeogharia@gmail.com>
2025-07-09 21:24:10 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,19 @@
+let a = 10
+let b = 20
+let sum = a+b
+console.log(sum)
+
+let c = 10
+let d = 20
+let sub = c-d
+console.log(sub)
+
+let e = 10
+let f = 20
+let mult = e*f
+console.log(mult)
+
+let g = 100
+let h = 2
+let div = g/h
+console.log(div)
